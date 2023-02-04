5c4b557693d6a2700a96d0af529c3a316e64e3df
joshua <joshua@iopewq.uk>
2023-02-04 20:32:16 +0000

    Created Hero template

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+
+
+
+// VARIABLE DATA
+
+var businessName = "Business Name!";
+var businessTagline = "Business Tagline Here!";
+var CTA1 = "Call To Action!";
+
+
+
+
+
+
+
+
+function loadFunction () {
+    document.getElementById("business-name").innerHTML = businessName;
+    document.getElementById("business-tagline").innerHTML = businessTagline;
+    document.getElementById("CTA-1").innerHTML = CTA1;
+    // alert("Theme Applied!");
+}
+// document.onload = loadFunction();
